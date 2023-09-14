6c75f51528739ade68294f9f1c28997d9ff7f7c6
Iacopo Ciao <iacopociao1992@gmail.com>
2023-09-14 12:30:53 +0200

    ci(classList): create readme class list generator

--- README.md
+++ README.md
@@ -4,15 +4,12 @@ Dungeon World è un gioco di ruolo da tavolo.
 In questo repository troverai i libretti standard e fanmade tradotti in italiano in formato PDF e i relativi file [InDesign](http://www.adobe.com/products/indesign.html).
 
 ## Classi
-### Standard
-WIP
-### Community
-
+<!-- START_CLASSLIST -->
 - Peter Johansen
   - Lore and Lords Pack
     - Berserker [[pdf](https://github.com/KernelPanic92/dungeonworld-ita/raw/master/classi/community/Peter%20Johansen/Lore%20and%20Lords%20Pack/berserker/berserker.pdf)] [[indd](https://github.com/KernelPanic92/dungeonworld-ita/raw/master/classi/community/Peter%20Johansen/Lore%20and%20Lords%20Pack/berserker/berserker.indd)]
     - Trickster [[pdf](https://github.com/KernelPanic92/dungeonworld-ita/raw/master/classi/community/Peter%20Johansen/Lore%20and%20Lords%20Pack/trickster/trickster.pdf)] [[indd](https://github.com/KernelPanic92/dungeonworld-ita/raw/master/classi/community/Peter%20Johansen/Lore%20and%20Lords%20Pack/trickster/trickster.indd)]
-
+<!-- END_CLASSLIST -->
 ## Licenza
 Guarda il file di [licenza](./LICENSE) per maggiori dettagli, ma ecco le nozioni di base: sei libero di utilizzare questo testo come preferisci purché citi gli autori.
 
---
+++ scripts/update-class-list-readme.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+
+const githubBaseUrl =
+  'https://github.com/KernelPanic92/dungeonworld-ita/raw/master/classi';
+
+
+function calculate() {
+  const directoryPath = process.env.GITHUB_WORKSPACE + '/classi';
+  const relative = process.env.GITHUB_WORKSPACE + '/classi';
+
+  const markdown = [];
+  createMarkdown(directoryPath, relative,  markdown, 0);
+  updateReadme(markdown.join('\n'));
+}
+
+function updateReadme(classList) {
+  const inputFilePath = 'README.md';
+
+  if (!fs.existsSync(inputFilePath)) {
+    console.log(`File not found: ${inputFilePath}`);
+    return;
+  }
+
+  const inputContent = fs.readFileSync(inputFilePath, 'utf8');
+  const pattern = /<!-- START_CLASSLIST -->(.*?)<!-- END_CLASSLIST -->/s;
+  const replacedContent = inputContent.replace(
+    pattern,
+    `<!-- START_CLASSLIST -->\n${classList}\n<!-- END_CLASSLIST -->`
+  );
+
+  fs.writeFileSync(inputFilePath, replacedContent);
+  console.log(`Content between comments replaced and saved to ${inputFilePath}`);
+}
+
+function createMarkdown(currentDirectory, relative, markdown, level) {
+  const fileSystemEntities = fs
+    .readdirSync(currentDirectory, { withFileTypes: true })
+    .filter((entity) => !entity.name.startsWith('.'))
+    .sort((left, right) => left.name.localeCompare(right.name));
+
+  const files = fileSystemEntities.reduce((acc, entity) => {
+    if (entity.isFile()) {
+      const name = path.basename(entity.name, path.extname(entity.name));
+      const extension = path.extname(entity.name).slice(1);
+      acc[name] = acc[name] ?? {};
+      acc[name][extension] = path.join(currentDirectory, entity.name);
+    }
+    return acc;
+  }, {});
+
+  const directories = fileSystemEntities.filter((entity) => entity.isDirectory());
+  const padding = ' '.repeat(level * 2);
+
+  for (const [name, extensions] of Object.entries(files)) {
+    let row = `${padding}- ${name}`;
+    for (const [extension, filePath] of Object.entries(extensions)) {
+      const url = encodeURI(`${githubBaseUrl}${filePath}`.replace(relative, ''));
+      row += ` [${extension}](${url})`;
+    }
+    markdown.push(row);
+  }
+
+  for (const directory of directories) {
+    const name = path.basename(directory.name);
+    markdown.push(`${padding}- ${name}`);
+    createMarkdown(path.join(currentDirectory, directory.name), relative, markdown, level + 1);
+  }
+}
+
+calculate();
